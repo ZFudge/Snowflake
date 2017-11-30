98fd63521f894a497de949b8562c8aee7f3bf11b
zfudge <zacheryfudge@gmail.com>
2017-11-30 03:41:28 -0700

    created control box

--- snow.js
+++ snow.js
@@ -10,7 +10,7 @@ const snow = {
   skew: 0,
   outerAngle: 30,
   asynch: false,
-  asyncDelay: 5,
+  asynchDelay: 30,
   colors: ['#870c8c', '#5f09c1', '#202ccc', '#2bc4bc', '#33b220', '#f78b32', '#f73347']
 };
 
@@ -35,9 +35,8 @@ async function iceSeed() {
 
 async function dendrites(x, y, tier, len, wid, c) {
   if (tier > 0) {
-    if (snow.asynch) await timeout(snow.asyncDelay);
-    context.strokeStyle = `rgb(${c},${c},${c})`;
-    //context.strokeStyle = snow.colors[tier];
+    if (snow.asynch) await timeout(snow.asynchDelay);
+    context.strokeStyle = `rgb(${c},${c},${c})`;//context.strokeStyle = snow.colors[tier];
     context.lineWidth = wid;
     
     context.save();
@@ -88,50 +87,66 @@ function recreate() {
   iceSeed();
 }
 
-const dendSlide = document.getElementById('dend');
-function adjustDendrites(newDend) {
-  snow.dendrites = newDend;
-  recreate();
-}
-
 const tierSlide = document.getElementById('tiers');
+const tierValue = document.getElementById('tier-value');
 function adjustTier(newTiers) {
+  tierValue.innerHTML = newTiers;
   snow.tiers = newTiers;
   recreate();
 }
 
+const dendSlide = document.getElementById('dend');
+const dendValue = document.getElementById('dendrite-value');
+function adjustDendrites(newDend) {
+  dendValue.innerHTML = newDend;
+  snow.dendrites = newDend;
+  recreate();
+}
+
 const angleSlide = document.getElementById('angle');
+const angleValue = document.getElementById('angle-value');
 function adjustAngle(newAng) {
+  angleValue.innerHTML = parseFloat(newAng).toFixed(1) + ' Deg';
   snow.outerAngle = newAng;
   recreate();
 }
 
 const skewSlide = document.getElementById('skew');
+const rotationValue = document.getElementById('rotation-value');
 function adjustSkew(newSkew) {
+  rotationValue.innerHTML = parseFloat(newSkew).toFixed(1) + ' Deg';
   snow.skew = newSkew;
   recreate();
 }
 
 const widthSlide = document.getElementById('width');
+const widthValue = document.getElementById('width-value');
 function adjustWidth(newWidth) {
+  widthValue.innerHTML = newWidth;
   snow.width = newWidth;
   recreate();
 }
 
 const lateralLengthSlide = document.getElementById('lateralLength');
+const lateralValue = document.getElementById('lateral-value');
 function adjustLateralLength(newLatLen) {
+  lateralValue.innerHTML = parseFloat(newLatLen * 100).toFixed(1);
   snow.lateralLength = newLatLen;
   recreate();
 }
 
 const apicalLengthSlide = document.getElementById('apicalLength');
+const apicalValue = document.getElementById('apical-value');
 function adjustApicalLength(newApicLen) {
+  apicalValue.innerHTML = parseFloat(newApicLen * 100).toFixed(1);
   snow.apicalLength = newApicLen;
   recreate();
 }
 
 const lengthSlide = document.getElementById('length');
+const lengthValue = document.getElementById('length-value');
 function adjustLength(newLen) {
+  lengthValue.innerHTML = newLen;
   snow.length = newLen;
   recreate();
 }
@@ -139,6 +154,13 @@ function adjustLength(newLen) {
 const asyncBtn = document.getElementById('asynch');
 function asynchSwitch() {
   snow.asynch = !snow.asynch;
+}
+
+const asyncDelaySlide = document.getElementById('asynchDelay');
+const asyncDelayValue = document.getElementById('async-delay-value');
+function adjustAsynchDelay(newAsynDel) {
+  asyncDelayValue.innerHTML = newAsynDel;
+  snow.asynchDelay = newAsynDel;
 }
 
 iceSeed();
